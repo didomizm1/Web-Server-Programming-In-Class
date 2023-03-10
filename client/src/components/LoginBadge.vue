ef487bda23b7454a2991ebda750ea7bbcb9b5436
didomizm1 <didomizm1@newpaltz.edu>
2023-03-09 20:34:42 -0500

    Navbar and login badge updates

--- client/src/components/LoginBadge.vue
+++ client/src/components/LoginBadge.vue
@@ -1,31 +1,34 @@
 <script setup lang="ts">
-import { useSession, login } from '@/model/session';
 import { ref } from 'vue';
+import { useSession, login, useLogout } from '@/model/session';
 
 const session = useSession();
+const logout = useLogout();
 
+function logout2() {
+    logout();
+}
 </script>
 
 <template>
-    <div class="navbar-item" v-if="session.user">
-        Welcome, {{ session.user.name }}
-        (<a @click="$event => session.user = null">Logout</a>)
-    </div>
-    <div class="navbar-item" v-else>
-        <a class="button is-primary" @click="login">
-            <span class="icon">
-                <i class="fas fa-user"></i>
-            </span>
-            <strong>Login</strong>
-        </a>
-        <a class="button is-info">
-            <span class="icon">
-                <i class="fas fa-user-plus"></i>
-            </span>
-            <strong>Sign up</strong>
-        </a>
-    </div>
-
+            <div class="navbar-item" v-if="session.user">
+                Welcome, {{ session.user.name }}
+                (<a @click="logout2()">logout</a>)
+            </div>
+            <div class="navbar-item" v-else>
+                <a class="button is-primary" @click="login">
+                    <span class="icon">
+                        <i class="fas fa-user"></i>
+                    </span>
+                    <strong>Login</strong>
+                </a>
+                <a class="button is-info">
+                    <span class="icon">
+                        <i class="fas fa-user-plus"></i>
+                    </span>
+                    <strong>Sign up</strong>
+                </a>
+            </div>
 </template>
 
 <style scoped>
