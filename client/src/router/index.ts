e0cad8da40645c36541f2eb3de243251bf016566
didomizm1 <didomizm1@newpaltz.edu>
2023-03-09 20:35:36 -0500

    Routing update

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type NavigationGuardNext, type RouteLocationNormalized } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProductsVue from '@/views/Products.vue'
 import LoginVue from '@/views/Login.vue'
@@ -23,13 +23,11 @@ const router = createRouter({
 
 export default router
 
-function secureRoute (to, from, next) {
-  const session = useSession
-  if (session.user)
-  {
-    next()
-  }
-  else {
-    next('/login')
+function secureRoute (to : RouteLocationNormalized, from : RouteLocationNormalized, next : NavigationGuardNext ) {
+  const session = useSession();
+  if (session.user) {
+      next()
+  } else { 
+      next('/login')
   }
 }
